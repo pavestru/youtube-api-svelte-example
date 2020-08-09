2cfa737a1e2cda98117708ae8ad23e02f4cafd7a
Pavel Struhar <pavel.struhar@accenture.com>
2020-08-09 11:15:30 +0200

    Extract Video component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
 
+  import Video from "./Video.svelte";
   import { parseYTDuration, filterTags, sortVideos } from "./utils.js";
 
   const THIRTY_ONE_DAYS_IN_MS = 31 * 24 * 3600 * 1000;
@@ -54,71 +55,6 @@
     display: flex;
     flex-wrap: wrap;
   }
-
-  .App__container .video {
-    width: 230px;
-    padding: 10px;
-    text-align: center;
-    margin-bottom: 20px;
-  }
-
-  .App__container .video a {
-    display: block;
-    position: relative;
-  }
-
-  .App__container .video a:hover {
-    text-decoration: none;
-  }
-
-  .thumb {
-    height: 129px;
-  }
-
-  .duration {
-    height: 20px;
-    padding: 0 5px;
-    line-height: 20px;
-    text-align: right;
-    text-decoration: none;
-    color: white;
-    position: absolute;
-    right: 5px;
-    bottom: 5px;
-    background-color: black;
-    box-sizing: border-box;
-    font-size: 80%;
-    border-radius: 3px;
-  }
-
-  .title {
-    padding: 3px;
-    text-align: left;
-    color: #333;
-    font-size: 90%;
-    font-weight: 600;
-  }
-
-  .App__container .video img {
-    max-width: 100%;
-  }
-
-  .tags {
-    text-align: left;
-    padding-top: 3px;
-    font-size: 85%;
-  }
-
-  .tag {
-    color: white;
-    background-color: gray;
-    padding: 0.2ex 0.5ex;
-    border-radius: 3px;
-  }
-
-  .new {
-    background-color: black;
-  }
 </style>
 
 <div class="App__container">
@@ -126,29 +62,7 @@
     <div>Nahrávam...</div>
   {:else}
     {#each videos as { id, duration, tags, isNew, snippet: { title, thumbnails } } (id)}
-      <div class="video">
-        <a
-          class="thumb"
-          href={`https://youtube.com/watch?v=${id}`}
-          target="_blank">
-          <img alt={title} src={thumbnails.medium.url} />
-          <div class="duration">{duration}</div>
-        </a>
-        <a href={`https://youtube.com/watch?v=${id}`} target="_blank">
-          <div class="title">{title}</div>
-          {#if tags}
-            <div class="tags">
-              {#if isNew}
-                <span class="tag new">nové</span>
-              {/if}
-              {#each tags as tag (tag)}
-                <span class="tag">{tag}</span>
-                {' '}
-              {/each}
-            </div>
-          {/if}
-        </a>
-      </div>
+      <Video {id} {duration} {tags} {isNew} {title} {thumbnails} />
     {/each}
   {/if}
 </div>
---
+++ src/Video.svelte
@@ -0,0 +1,96 @@
+<script>
+  export let id;
+  export let duration;
+  export let tags;
+  export let isNew;
+  export let title;
+  export let thumbnails;
+</script>
+
+<style>
+  .video {
+    width: 230px;
+    padding: 10px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  .video a {
+    display: block;
+    position: relative;
+  }
+
+  .video a:hover {
+    text-decoration: none;
+  }
+
+  .thumb {
+    height: 129px;
+  }
+
+  .duration {
+    height: 20px;
+    padding: 0 5px;
+    line-height: 20px;
+    text-align: right;
+    text-decoration: none;
+    color: white;
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    background-color: black;
+    box-sizing: border-box;
+    font-size: 80%;
+    border-radius: 3px;
+  }
+
+  .title {
+    padding: 3px;
+    text-align: left;
+    color: #333;
+    font-size: 90%;
+    font-weight: 600;
+  }
+
+  .video img {
+    max-width: 100%;
+  }
+
+  .tags {
+    text-align: left;
+    padding-top: 3px;
+    font-size: 85%;
+  }
+
+  .tag {
+    color: white;
+    background-color: gray;
+    padding: 0.2ex 0.5ex;
+    border-radius: 3px;
+  }
+
+  .new {
+    background-color: black;
+  }
+</style>
+
+<div class="video">
+  <a class="thumb" href={`https://youtube.com/watch?v=${id}`} target="_blank">
+    <img alt={title} src={thumbnails.medium.url} />
+    <div class="duration">{duration}</div>
+  </a>
+  <a href={`https://youtube.com/watch?v=${id}`} target="_blank">
+    <div class="title">{title}</div>
+    {#if tags}
+      <div class="tags">
+        {#if isNew}
+          <span class="tag new">nové</span>
+        {/if}
+        {#each tags as tag (tag)}
+          <span class="tag">{tag}</span>
+          {' '}
+        {/each}
+      </div>
+    {/if}
+  </a>
+</div>
